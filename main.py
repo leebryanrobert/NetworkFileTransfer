ae6c4c5a14a94a95114dbada2ad2b85467e85b9d
Brendan L <brenlieb2@gmail.com>
2020-12-07 16:00:10 -0600

    changed all command line interaction to gui interaction
    got rid of print statements and replaced with gui functionality

--- main.py
+++ main.py
@@ -1,116 +1,251 @@
-import os
+import PySimpleGUI as sg
+import ctypes
 import re
+import fnmatch
+import os
 import socket
 import tqdm
+
 
 IP_REG = '^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$'
 PORT_REG = '^6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|5\d{4}|[0-9]\d{0,3}$'
 SEPARATOR = '@@@'
 BUFFER_SIZE = 4096
 
-choice = 'n'
-while choice not in 'sr':
-	print('(S)ender or (R)eciever?')
-	choice = input()
-	while not choice:
-		choice = input()
-	choice = choice[0].lower()
+user32 = ctypes.windll.user32
+Width = user32.GetSystemMetrics(0)
+Height = user32.GetSystemMetrics(1)
+
+sg.theme('DarkPurple4')
 
 
-#	Client Code
-if choice == 's':
-	print('You have chosen role: "Client/Sender"')
-
-	print('\nPlease enter the IPv4 address that you wish to send to...')
-	host = input()
-
-	while re.fullmatch(IP_REG, host) == None:
-		print('Invalid IPv4 address, try again...')
-		host = input()
-
-	print('\nPlease enter the port that you wish to send to...')
-	port = input()
-
-	while re.fullmatch(PORT_REG, port) == None:
-			print('Invalid port, try again...')
-			port = input()
+layout = [[sg.Button("HOST",size=(20,5))] , [sg.Button("CLIENT", size=(20,5))]]
+window = sg.Window("Hello World",layout ,margins =(Width/10,Height/5))
+event, values = window.read()
 	
-	port = int(port)
 
-	print('\nPlease enter the path/name of the file you with to send...')
-	file_name = input()
-
-	while not os.path.isfile(file_name):
-		print('File does not exist, try again...')
-		file_name = input()
-
-	file_size = os.path.getsize(file_name)
-
-	sock = socket.socket()
-	sock.connect((host, port))
-
-	sock.send(f"{file_name}{SEPARATOR}{file_size}".encode())
-
-	progress = tqdm.tqdm(range(file_size), "Sending {}".format(file_name), unit='B', unit_scale=True, unit_divisor=1024)
-	with open(file_name, 'rb') as file:
-		for _ in progress:
-			bytes_read = file.read(BUFFER_SIZE)
-			if not bytes_read:
-				sock.close()
-				break
-			sock.sendall(bytes_read)
-			progress.update(len(bytes_read))
-
-
-#	Server Code
-else:
-	print('You have chosen role: "Server/Receiver"\n')
-
+if event == "HOST":
 	host = '0.0.0.0'
+	error=False
+	cont = True
+	window.close()
+	hostlayout = [[sg.Text("Port", size=(5,1)), sg.InputText()], [sg.Submit(), sg.Cancel()]]
+	errorlayout = [[sg.Text("Port", size=(5,1)), sg.InputText()], [sg.Text("Error, invalid port number!")], [sg.Submit(), sg.Cancel()]]
+	window = sg.Window("host",layout=hostlayout,margins=(200,200))
+	while True:
+		event, values = window.read()
+		if event == sg.WIN_CLOSED or event == "Cancel":
+			cont = False
+			break
 
-	print('\nPlease enter the port that you wish to use...')
-	port = input()
+		#input validation for port number
+		while re.fullmatch(PORT_REG, values[0]) == None:
+			if error==False:
+				window.close()
+				window = sg.Window("host",layout=errorlayout,margins=(200,200))
+			error=True
 
-	while re.fullmatch(PORT_REG, port) == None:
-	    print('Invalid port, try again...')
-	    port = input()
-
-	port = int(port)
-
-	server = socket.socket()
-
-	server.bind((host, port))
-
-	print('\nWaiting for connection...')
-
-	server.listen(5)
-
-	client, address = server.accept()
-
-	received = client.recv(BUFFER_SIZE).decode()
-
-	file_name, file_size = received.split(SEPARATOR)
-
-	file_name = os.path.basename(file_name)
-
-	file_size = int(file_size)
-
-	progress = tqdm.tqdm(range(file_size), '\nReceiving {}'.format(file_name), unit='B', unit_scale=True, unit_divisor=1024)
-
-	with open(file_name, 'wb') as file:
-
-		for _ in progress:
-			bytes_read = client.recv(BUFFER_SIZE)
-
-			if not bytes_read:
-				client.close()
-				server.close()
+			if event == "Cancel":
+				cont = False
 				break
+		
+			event, values = window.read()
+		
+		#port = values[0]
+		#port = int(port)
+		break
 
-			file.write(bytes_read)
+	#program will not continue if cancel is pressed
+	if cont == True:
+		port = values[0]
+		port = int(port)
+		server = socket.socket()
+		server.bind((host,port))
 
-			progress.update(len(bytes_read))
+		window.close()
 
-print('Transfer success!')
-print('Press any key to exit...')
-input()
+		#displays window that shows user which port they are listening on for a few seconds
+		message = "Waiting on port " + str(port)
+		sentlayout = [[sg.Text(message)]]
+		window = sg.Window("",layout=sentlayout,margins=(20,20))
+
+		event, values = window.read(timeout=3000)
+		window.close()
+		
+		server.listen(5)
+		
+		client, address = server.accept()
+
+		received = client.recv(BUFFER_SIZE).decode()
+
+		file_name, file_size = received.split(SEPARATOR)
+
+		file_name = os.path.basename(file_name)
+
+		file_size = int(file_size)
+
+		receivinglayout=[[sg.Text("Recieving file, Please Wait...")]]
+		window = sg.Window("",receivinglayout,margins=(20,20),location=(Width/2,Height/3))
+		event, values = window.read(timeout=2000)
+		window.close()
+
+		progress = tqdm.tqdm(range(file_size), '\nReceiving {}'.format(file_name), unit='B', unit_scale=True, unit_divisor=1024)
+
+		with open(file_name, 'wb') as file:
+
+			for _ in progress:
+				bytes_read = client.recv(BUFFER_SIZE)
+
+
+				if not bytes_read:
+					client.close()
+					server.close()
+					break
+
+				file.write(bytes_read)
+
+				progress.update(len(bytes_read))
+				
+		window.close()
+
+		donelayout = [[sg.Text("File Recieved Succesfully!")]]
+		event, values = sg.Window("",donelayout,margins=(20,20),location=(Width/4,(Height/2)-100)).read(timeout=4000)
+	
+ 
+
+
+#code for client side
+if event == "CLIENT":
+	cont = True
+	window.close()
+	ipv4error = False
+	porterror = False
+	
+	clientlayout = [[sg.Text("IPv4", size=(5,1)), sg.InputText()], [sg.Text("Port", size=(5,1)), sg.InputText()] ,[sg.Submit(), sg.Cancel()]]
+	ipv4errorlayout = [[sg.Text("IPv4", size=(5,1)), sg.InputText()], [sg.Text("Port", size=(5,1)), sg.InputText()], [sg.Text("ipv4 is not a valid ipv4 address")] ,[sg.Submit(), sg.Cancel()]]
+	porterrorlayout = [[sg.Text("IPv4", size=(5,1)), sg.InputText()], [sg.Text("Port", size=(5,1)), sg.InputText()], [sg.Text("port is not a valid port number")] ,[sg.Submit(), sg.Cancel()]]
+	window = sg.Window("client",clientlayout,margins=(200,200))
+	
+	#event loop to get ipv4 and port values
+	while True:
+		event, values = window.read()
+		if event == sg.WIN_CLOSED or event == "Cancel":
+			cont = False
+			break
+		
+		#checks ipv4 input for a valid ipv4 address
+		while re.fullmatch(IP_REG, values[0]) == None:
+			if ipv4error == False:
+				window.close()
+				window = sg.Window("client",ipv4errorlayout,margins=(200,200))
+				ipv4error = True
+			
+			if event == "Cancel":
+				cont = False
+				break
+			event, values = window.read()
+		
+		if cont == False:
+			break
+		host = values[0]
+
+		#checks port input for valid port number
+		while re.fullmatch(PORT_REG, values[1]) == None:
+			if porterror == False:
+				window.close()
+				window = sg.Window("client",porterrorlayout,margins=(200,200))
+				porterror=True
+
+			if event == "Cancel":
+				cont = False
+				break
+			event, values = window.read()
+		
+		port = values[1]
+		port = int(port)
+		break
+
+	window.close()
+
+	#only progresses program if ipv4 and port were valid inputs
+	if cont == True:
+		file_list_column = [
+			[
+				sg.Text("Image Folder"),
+				sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+				sg.FolderBrowse(),
+				sg.Button("Send")
+			],
+			[
+				sg.Listbox(
+					values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+				)
+			],
+		]
+
+		layout = [[sg.Column(file_list_column)]]
+		window = sg.Window("file select", layout)
+		filename = ''
+		while True:
+			event, values = window.read()
+			if event == sg.WIN_CLOSED:
+				break
+			if event == "-FOLDER-":
+				folder = values["-FOLDER-"]
+				try:
+					# Get list of files in folder
+					file_list = os.listdir(folder)
+				except:
+					file_list = []
+				fnames = [
+					f
+					for f in file_list
+					if os.path.isfile(os.path.join(folder, f))
+				]
+				window["-FILE LIST-"].update(fnames)
+			elif event == "-FILE LIST-":  # A file was chosen from the listbox
+				try:
+					filename = os.path.join(
+						values["-FOLDER-"], values["-FILE LIST-"][0]
+					)
+					
+					print(filename)
+					print(len(filename))
+				except:
+					pass
+			print(len(filename))
+			if event == "Send" and len(filename) != 0:
+				break
+			elif event == "Send" and len(filename) == 0:
+				print("Error nothing selected")
+
+		filesent=False
+		#attempt to send file after file is chosen
+		if len(filename) != 0:
+			file_size = os.path.getsize(filename)
+
+			sock = socket.socket()
+			sock.connect((host, port))
+			
+			sock.send(f"{filename}{SEPARATOR}{file_size}".encode())
+
+			progress = tqdm.tqdm(range(file_size), "Sending {}".format(filename), unit='B', unit_scale=True, unit_divisor=1024)
+			with open(filename, 'rb') as file:
+				for _ in progress:
+					bytes_read = file.read(BUFFER_SIZE)
+					if not bytes_read:
+						sock.close()
+						break
+					sock.sendall(bytes_read)
+					progress.update(len(bytes_read))
+				print(filename)
+			filesent=True
+
+		window.close()
+		if filesent==True:
+			endlayout = [[sg.Text("File Sent. Program will now close")]]
+			window = sg.Window("Client End",endlayout,margins=(20,20))
+			event, values = window.read(timeout = 3000)
+			window.close()
+
+
